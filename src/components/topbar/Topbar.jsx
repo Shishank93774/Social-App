c3d98f386a8279f6281688d44399632678d3717b
Shishank <shishankrawat93774@gmail.com>
2023-03-01 12:44:29 +0530

    init files

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,47 @@
+import "./topbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+
+const Topbar = () => {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="topbarLogo">Hanksy</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" fontSize="small" />
+          <input
+            placeholder="search for Friends, Posts, or Video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">HomePage</span>
+          <span className="topbarLink">TimeLine</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/4.jpg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
